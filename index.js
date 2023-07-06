b13c59bf756fd2b04706f5a5a597996646013749
zhangyibo.114514 <zhangyibo.114514@bytedance.com>
2023-07-06 14:48:12 +0800

    add lingness

--- index.js
+++ index.js
@@ -1,30 +1,31 @@
 const {
-    ProcessText,
-    RandomLing
+	ProcessText,
+	RandomLing,
+	RandomBigMuLingNess,
 } = require("./handler")
 const {
-    Telegraf
+	Telegraf
 } = require('telegraf');
 const {
-    message
+	message
 } = require('telegraf/filters');
 const {
-    Application,
-    Router
+	Application,
+	Router
 } = require('@cfworker/web');
 const createTelegrafMiddleware = require('cfworker-middleware-telegraf');
 
 
 function hash(str) {
-    var hash = 0,
-        i, chr;
-    if (str.length === 0) return hash;
-    for (i = 0; i < str.length; i++) {
-        chr = str.charCodeAt(i);
-        hash = ((hash << 5) - hash) + chr;
-        hash |= 0; // Convert to 32bit integer
-    }
-    return hash;
+	var hash = 0,
+		i, chr;
+	if (str.length === 0) return hash;
+	for (i = 0; i < str.length; i++) {
+		chr = str.charCodeAt(i);
+		hash = ((hash << 5) - hash) + chr;
+		hash |= 0; // Convert to 32bit integer
+	}
+	return hash;
 }
 
 
@@ -34,35 +35,45 @@ const bot = new Telegraf(self.BOT_TOKEN);
 // Do not forget to set environment variables BOT_TOKEN and SECRET_PATH on your worker
 bot.on(message("text"), ctx => ctx.reply("这是一个inline bot，输入 @ling_lang_bot 以使用"));
 bot.on('inline_query', async (ctx) => {
-    const result = [];
-    let query = ctx.inlineQuery.query;
-    if (!query) {
-        const text = RandomLing()
-        result.push({
-            type: "article",
-            id: hash(text),
-            title: "随机一淋",
-            description: "随机一淋",
-            input_message_content: {
-                message_text: text
-            },
-        })
-    } else {
-        const newText = ProcessText(query)
-        result.push({
-            type: "article",
-            id: hash(query),
-            title: "啾咪",
-            description: newText,
-            input_message_content: {
-                message_text: newText
-            },
-        })
-    }
-    // Using context shortcut
-    await ctx.answerInlineQuery(result, {
-        cache_time: 0
-    });
+	const result = [];
+	let query = ctx.inlineQuery.query;
+	if (!query) {
+		const lingness = RandomBigMuLingNess();
+		const randomLing = RandomLing();
+		result.push({
+			type: "article",
+			id: hash(randomLing),
+			title: "随机一淋",
+			description: "随机一淋",
+			input_message_content: {
+				message_text: randomLing
+			},
+		})
+		result.push({
+			type: "article",
+			id: hash(lingness),
+			title: "测测我的零指数",
+			description: "测测我的零指数",
+			input_message_content: {
+				message_text: lingness
+			},
+		})
+	} else {
+		const newText = ProcessText(query)
+		result.push({
+			type: "article",
+			id: hash(query),
+			title: "啾咪",
+			description: newText,
+			input_message_content: {
+				message_text: newText
+			},
+		})
+	}
+	// Using context shortcut
+	await ctx.answerInlineQuery(result, {
+		cache_time: 0
+	});
 });
 
 const router = new Router();
